57c07f8b778a9d76f03c62f11953a4ef65430039
Ilona Eisenbraun <ilona.eisenbraun@hhi.fraunhofer.de>
2021-02-23 00:26:56 +0100

    intermediate selection

--- src/components/Sidebar/ImgBox.js
+++ src/components/Sidebar/ImgBox.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
 
 import useCanvas from './useCanvas';
 import { Card, makeStyles } from '@material-ui/core';
@@ -14,17 +16,27 @@ const useStyles = makeStyles(theme => ({
   side: {
     width: '50%'
   },
-  img: {
-    height: '100%',
-    width: '100%',
-    objectFit: 'cover',
-    flex: 1
+  crop: {
+    display: 'block'
   }
 }));
 
 const ImgBox = ({ viewType, content, isToggled, watershed, title }) => {
   const classes = useStyles();
-  const [setCoordinates, canvasRef] = useCanvas();
+  const [setCoordinates, canvasRef] = React.useState();
+  const [ratio, setRatio] = React.useState(0);
+  const [x, setX] = React.useState(0);
+  const [y, setY] = React.useState(0);
+  const [width, setWidth] = React.useState(0);
+  const [height, setHeight] = React.useState(0);
+
+  const [crop, setCrop] = React.useState({
+    x: 0,
+    y: 0,
+    unit: 'px',
+    width: 0,
+    height: 0
+  });
 
   const handleCanvasClick = () => {
     const canvas = canvasRef.current;
@@ -46,27 +58,90 @@ const ImgBox = ({ viewType, content, isToggled, watershed, title }) => {
     const dataURL = canvas.toDataURL();
     setCoordinates([dataURL]);
   };
-  React.useEffect(() => {
-    if (isToggled === true && title === 'heatmap' && watershed) {
-      console.log(title);
-      handleCanvasClick();
-    }
-  }, [isToggled]);
 
-  React.useEffect(() => {
-    if (isToggled === false) {
-      setCoordinates([content]);
+  const onImageLoaded = image => {
+    setRatio(image.clientHeight / 28);
+    setCrop({
+      x: 0,
+      y: 0,
+      width: image.clientHeight,
+      height: image.clientHeight
+    });
+  };
+
+  const onCropComplete = crop => {
+    setX(crop.x / ratio);
+    setY(crop.y / ratio);
+    setWidth(crop.width / ratio);
+    setHeight(crop.height / ratio);
+  };
+
+  const onCropChange = crop => {
+    setCrop(crop);
+  };
+
+  function getCroppedImg(image, crop, fileName) {
+    const canvas = document.createElement('canvas');
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+    canvas.width = crop.width;
+    canvas.height = crop.height;
+    const ctx = canvas.getContext('2d');
+    console.log(canvas.width);
+
+    ctx.drawImage(
+      image,
+      crop.x * scaleX,
+      crop.y * scaleY,
+      crop.width * scaleX,
+      crop.height * scaleY,
+      0,
+      0,
+      crop.width,
+      crop.height
+    );
+
+    return new Promise((resolve, reject) => {
+      canvas.toBlob(blob => {
+        if (!blob) {
+          //reject(new Error('Canvas is empty'));
+          console.error('Canvas is empty');
+          return;
+        }
+        blob.name = fileName;
+        window.URL.revokeObjectURL(this.fileUrl);
+        this.fileUrl = window.URL.createObjectURL(blob);
+        resolve(this.fileUrl);
+      }, 'image/jpeg');
+    });
+  }
+
+  async function makeClientCrop(crop) {
+    if (canvasRef && crop.width && crop.height) {
+      const croppedImageUrl = await getCroppedImg(
+        canvasRef,
+        crop,
+        'newFile.jpeg'
+      );
+      this.setState({ croppedImageUrl });
     }
-  }, [content, isToggled]);
+  }
   return (
     <Card className={viewType === 'DEFAULTVIEW' ? classes.root : classes.side}>
-      <canvas
-        id={title}
-        className={classes.img}
-        ref={canvasRef}
-        width={canvasWidth}
-        height={canvasHeight}
-      />
+      {title === 'original' ? (
+        <ReactCrop
+          className={classes.crop}
+          imageStyle={{ height: '100%', width: '100%', objectFit: 'cover' }}
+          src={content}
+          crop={crop}
+          ruleOfThirds
+          onImageLoaded={onImageLoaded}
+          onComplete={onCropComplete}
+          onChange={onCropChange}
+        />
+      ) : (
+        <div></div>
+      )}
     </Card>
   );
 };
